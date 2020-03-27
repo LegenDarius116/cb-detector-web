d4450fb4a45d752fdd9dc1b3bc84829638cc99f5
Darius <darius.sanagustin116@gmail.com>
2020-03-27 11:57:30 -0400

    Changed model for better prediction numbers. Added aesthehic changes.

--- cb_detector_web/main/views.py
+++ cb_detector_web/main/views.py
@@ -7,7 +7,9 @@ from joblib import load
 def index(request):
     context = {}
 
-    if request.method == 'POST':
+    if request.method == 'GET':
+        context['form'] = TextForm()
+    elif request.method == 'POST':
         form = TextForm(request.POST)
         if form.is_valid():
             # get the content of entered text
@@ -16,11 +18,31 @@ def index(request):
             # load the machine learning model
             text_clf = load('main/model/model_v1.model')
 
+            # predict if text is bullying or not (binary)
             bool_result = text_clf.predict([body])[0] == 1
-            probability_result = text_clf.predict_proba([body])[0]
 
+            # show confidence that the prediction is indeed cyberbullying
+            probability_result = text_clf.predict_proba([body])[0][1]*100
+
+            # rounding probability result to 2 decimal places (in percent form
+            probability_result = round(100*probability_result) / 100
+
+            # pick color of alert for toxicity rating (confidence)
+            if probability_result < 30:
+                toxicity_rate_alert_color = "alert alert-success"
+            elif 30 <= probability_result < 50:
+                toxicity_rate_alert_color = "alert alert-warning"
+            else:
+                toxicity_rate_alert_color = "alert alert-danger"
+
+            # pick color of alert for binary prediction
+            bullying_prediction_alert_color = "alert alert-success" if not bool_result else "alert alert-danger"
+
+            # add these to context object
+            context['toxicity_rate_alert_color'] = toxicity_rate_alert_color
             context['bully_prediction'] = bool_result
-            context['bully_probability'] = "{0:.2f}".format(probability_result[1]*100)
+            context['bully_probability'] = probability_result
+            context['bullying_prediction_alert_color'] = bullying_prediction_alert_color
+            context['form'] = form
 
-    context['form'] = TextForm()
     return render(request, 'main/index.html', context)
